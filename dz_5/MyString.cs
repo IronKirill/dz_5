1669c9d3cd88343f4f33645ca08d074293402e67
kiryl <kiryl@DESKTOP-3T3FAOL>
2024-12-06 20:24:18 +0200

    Add project files.

---
+++ dz_5/MyString.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyString_
+{
+    public class MyString
+    {
+        public char[] Str { get; set; }
+        public int Length { get; }
+        public MyString(char[] str)
+        {
+            //this.str = str;
+            Str = str ?? throw new ArgumentNullException(nameof(str));
+        }
+
+        public char StartWith() => Str[0];
+        public char EndWith() => Str[Str.Length - 1];
+        public char[] Substring(int startIndex, int endIndex)
+        {
+            if (startIndex < 0 || endIndex < 0 || startIndex >= Str.Length || 
+                endIndex >= Str.Length || startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException("Invalid indices!");
+            }
+
+            int length = endIndex - startIndex + 1;
+            char[] result = new char[length];
+            Array.Copy(Str, startIndex, result, 0, length);
+            return result;
+        }
+        public void Remove(int startIndex, int endIndex)
+        {
+            if (startIndex < 0 || endIndex < 0 || startIndex >= Str.Length || 
+                endIndex >= Str.Length || startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException("Invalid indices!");
+            }
+
+            int lengthToRemove = endIndex - startIndex + 1;
+            char[] newStr = new char[Str.Length - lengthToRemove];
+            Array.Copy(Str, 0, newStr, 0, startIndex);
+            Array.Copy(Str, endIndex + 1, newStr, startIndex, Str.Length - endIndex - 1);
+            Str = newStr;
+        }
+        public void RemoveAll()
+        {
+            Str = Array.Empty<char>();
+        }
+        public int IndexOf(char[] search)
+        {
+            if (search == null || search.Length == 0)
+                throw new ArgumentNullException(nameof(search));
+
+            string str = new string(Str);
+            string search_Str = new string(search);
+            return str.IndexOf(search_Str, StringComparison.Ordinal);
+        }
+        public void Concat(char[] str1, char[] str2)
+        {
+            if (str1 == null || str2 == null)
+                throw new ArgumentNullException("Arguments can't be null!");
+
+            Str = new char[str1.Length + str2.Length];
+            Array.Copy(str1, 0, Str, 0, str1.Length);
+            Array.Copy(str2, 0, Str, str1.Length, str2.Length);
+        }
+        public void Replace(char[] newValue)
+        {
+            if (newValue == null)
+                throw new ArgumentNullException(nameof(newValue));
+
+            Str = new char[newValue.Length];
+            Array.Copy(newValue, Str, newValue.Length);
+        }
+        public void Replace2(char[] oldValue, char[] newValue)
+        {
+            if (oldValue == null || newValue == null)
+                throw new ArgumentNullException("Arguments can't be null!");
+
+            string str = new string(Str);
+            string oldStr = new string(oldValue);
+            string newStr = new string(newValue);
+            Str = (str.Replace(oldStr, newStr)).ToCharArray();
+        }
+        public void Trim(char ch)
+        {
+            string trim_Str = new string(Str).Replace(ch.ToString(), string.Empty);
+            Str = trim_Str.ToCharArray();
+        }
+        public bool CompareTo(char[] str1, char[] str2)
+        {
+            if (str1 == null || str2 == null)
+                throw new ArgumentNullException("Arguments can't be null!");
+
+            return new string(str1) == new string(str2);
+        }
+    }
+
+}
---
+++ dz_5/Program.cs
@@ -0,0 +1,79 @@
+﻿using MyString_;
+using System;
+
+namespace dz_5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = new char[] { 'H','E','L','L','O',',','W','O','R','L','D','!' };
+            //11(0-11)
+            MyString_.MyString myString = new MyString_.MyString(str);
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(myString.StartWith());
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(myString.EndWith());
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(myString.Substring(0,4));
+
+
+            Console.WriteLine("------------------------------------");
+            myString.Remove(0, 4);
+            Console.WriteLine(new string(myString.Str));
+
+
+            Console.WriteLine("------------------------------------");
+            myString.RemoveAll();
+            Console.WriteLine(new string(myString.Str));
+
+
+            Console.WriteLine("------------------------------------");
+            var str1 = new char[] { 'H', 'E', 'Y', ',' };
+            var str2 = new char[] { ' ', 'P', 'E', 'A', 'C', 'E', '!' };
+            myString.Concat(str1, str2);
+            Console.WriteLine(new string(myString.Str));
+
+
+            Console.WriteLine("------------------------------------");
+            myString.Replace(new char[] { 'G', 'O', 'O', 'D', 'B', 'Y', 'E' });
+            Console.WriteLine(new string(myString.Str));
+
+
+            Console.WriteLine("------------------------------------");
+            myString.Replace2(new char[] { 'O', 'O' }, new char[] { 'A','B' });
+            Console.WriteLine(new string(myString.Str));
+
+
+            Console.WriteLine("------------------------------------");
+            var trim = new MyString(new char[]
+            { 'H',' ','I',' ','!'});
+            trim.Trim(' ');
+            Console.WriteLine(new string(trim.Str));
+
+
+            Console.WriteLine("------------------------------------");
+            var str1_ = new char[] { 'H', 'E', 'L', 'L', 'O' };
+            var str2_ = new char[] { 'H', 'E', 'L', 'L', 'O' };
+            var str3_ = new char[] { 'W', 'O', 'R', 'L', 'D' };
+            Console.WriteLine(myString.CompareTo(str1_, str2_));
+            Console.WriteLine();
+            Console.WriteLine(myString.CompareTo(str1_, str3_));
+
+
+            Console.WriteLine("------------------------------------");
+            var search = new char[] { 'A', 'B' };
+            var indexOf = myString.IndexOf(search);
+            Console.WriteLine($"IndexOf ('{new string(search)}') = " + indexOf);
+            Console.WriteLine("------------------------------------");
+
+
+            Console.ReadKey();
+        }
+    }
+}
